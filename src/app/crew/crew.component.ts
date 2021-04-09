0e9eac913c063caedb7c6bb8bbf5adfe7e00de60
unknown <kdallen@accessus.net>
2021-04-09 12:49:14 -0500

    Studio completed except for Bonus

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -13,6 +13,8 @@ export class CrewComponent implements OnInit {
     {name: "Ellen Ochoa", firstMission: true}
   ];
 
+  memberBeingEdited: object = null;
+
   constructor() { }
 
   ngOnInit() {
@@ -20,6 +22,20 @@ export class CrewComponent implements OnInit {
 
   add(memberName: string, isFirst: boolean) {
     this.crew.push({name: memberName, firstMission: isFirst});
+  }
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+  }
+
+  save(name: string, member: object) {
+    member['name'] = name;
+    this.memberBeingEdited = null;
   }
 
 }
