01044f84ad0c8e97d618091cd2846a5d53292815
unknown <kdallen@accessus.net>
2021-04-11 14:42:44 -0500

    WIP Bonus

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipment: object[] = [
+    {part: 'Habitat dome'},
+    {part: 'Drones'},
+    {part: 'Food containers'},
+    {part:'Oxygen tanks'}
+  ];
+
+  editEquipment: object[] = null;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(part: string) {
+    this.equipment.push({part: part});
+  }
+
+  remove(part: object) {
+    let index = this.equipment.indexOf(part);
+    this.equipment.splice(index, 1);
+  }
+
+
+  edit(equipment: object) {
+    this.editEquipment = equipment;
   }
 
 }
